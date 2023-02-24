7474997b4fc9b59b42ab194305dce60dff76f472
Silver <silver@silv.re>
2023-02-24 03:32:47 +0000

    build: simply build.zig
    No real point in having target/optimize options just for tests

--- build.zig
+++ build.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
     b.addModule(.{
         .name = "zacc",
         .source_file = .{ .path = "src/zacc.zig" },
@@ -11,12 +8,8 @@ pub fn build(b: *std.Build) void {
 
     const tests = b.addTest(.{
         .root_source_file = .{ .path = "src/zacc.zig" },
-        .target = target,
-        .optimize = optimize,
     });
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&tests.step);
+    b.step("test", "Run library tests").dependOn(&tests.step);
 
     const exe = b.addExecutable(.{
         .name = "arithmetic",
